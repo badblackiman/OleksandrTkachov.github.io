f8bbf24c0e5eb087e327e142f9fc832977da6348
Tkachov Oleksandr <badblackiman@gmail.com>
2017-05-13 09:36:19 +0300

    hnya

--- Homework_js/js1_2/script.js
+++ Homework_js/js1_2/script.js
@@ -1,1 +1,22 @@
-test
+// alert('Что бы возвести число в степень, укажите число и степень');
+// function pow() {
+// 	var a = prompt('Enter the number');
+// 	var b = prompt('Enter the degree');
+// 	var i;
+// 	var x = a;
+// 	for (i=b-1; i > 0; i--){
+// 		x = x * a;
+// 	}
+// 	alert(x);
+// }
+// pow();
+
+// alert("Don't stop, go, go, go ...");
+
+
+// var n;
+// for (n=2; n > 0; n--){
+	// name.push = prompt('Enter any name');
+// }
+var name = [1, 2, 3, 4];
+alert(name.length);
