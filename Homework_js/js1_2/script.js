9b1e7086335479d9de8e58c8a0e4b78cd5f95be8
Tkachov Oleksandr <badblackiman@gmail.com>
2017-05-13 19:03:09 +0300

    Very done!

--- Homework_js/js1_2/script.js
+++ Homework_js/js1_2/script.js
@@ -3,9 +3,8 @@
 
 	//-----Возведение в степень-----
 	// Ввод и проверка целого числа
-	function integer(a) {
-		var text = a;
-		a = prompt(text);
+	function integer(text) {
+		var a = prompt(text);
 		while ((a ^ 0) != a){
 			alert ('Enter an integer!');
 			a = prompt(text);
@@ -13,64 +12,48 @@
 		return a;
 	}
 	//Возведение в положительную степень
-	function expoN(a,b) {
-		var x = a;
-		for (var i=1; i < b; i++){
-			x = x * a;
+	function expoN(num,expo) {
+		var x = num;
+		for (var i=1; i < expo; i++){
+			x *= num;
 		}
 		return x;
 	}
 	//Возведение в отрицательную и положительную степень
 	function pow() {
-		var a = integer('Enter the number');
-		var b = integer('Enter the degree');
-		if (b >= 0) {
-			var result = expoN(a,b);
-		} else {
-			b = -b;
-			result = 1 / expoN(a,b);
-		}
+		var num = integer('Enter the number'),
+				expo = integer('Enter the exponent');
+		if (expo == 0) {
+			result = 1;
+		} else if (expo > 0) {
+				var result = expoN(num,expo);
+			} else {
+			expo = -expo;
+				result = 1 / expoN(num,expo);
+			}
 		console.log('result =', result);
 	}
 	pow();
 
-
 	//-----Масив имён-----
-	var nameN = [];
-	var nameY;
 	function namE() {
+		var nameN = [];
 		for (var i=5; i > 0; i--){
 			nameN.push(prompt('Enter any name'));
 		}
-		nameY = prompt('Enter Your name');
+		var nameY = prompt('Enter Your name');
+		function search() {
+			for (var i=0; i < 5; i++){
+				if (nameN[i] == nameY) {
+					alert("Здравствуйте "+ nameY +", вы успешно вошли!");
+					return;
+				}
+			}
+			alert('Error!');
+		}
+		search();
 	}
 	namE();
-	//Вариант 1 - Хитрый =))
-	// var z = false;
-	// for (var i=0; i < 5; i++){
-	// 	if (nameN[i] == nameY) {
-	// 		alert(', вы успешно вошли');
-	// 		z = true;
-	// 	} else if (z == true){
-	// 		z = true;
-	// 	} else {
-	// 		z = false;
-	// 	}
-	// }
-	// if (z == false) alert('Error!');
-	//Вариант 1 - Конец
-	//Вариант 2 - Начало
-	function search() {
-		for (var i=0; i < 5; i++){
-			if (nameN[i] == nameY) {
-				alert("Здравствуйте "+ nameY +", вы успешно вошли!");
-				return;
-			}
-		}
-		alert('Error!');
-	}
-	search();
-	//Вариант 2 - Конец
 })();
 
 
